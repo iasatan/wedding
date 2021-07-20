5ace4cc22bb39b6fe3dec80a5adf9ab2004b5af9
Adam Satan <saa1mc@bosch.com>
2021-07-20 08:47:00 +0200

    about page

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+})
 app.listen(port, () => console.log('Server running on ' + port + "..."));
